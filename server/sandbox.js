6e9bcb0d0043fe1616417bc1406651b196d27a72
Stephane0305 <stephane.lalisse@devinci.fr>
2021-03-01 15:45:33 +0100

    shop: scrape new products

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,15 +1,13 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox () {
   try {
-    console.log(`🕵️‍♀️  browsing ${eshop} source`);
-
-    const products = await dedicatedbrand.scrape(eshop);
-
-    console.log(products);
-    console.log('done');
-    process.exit(0);
+    const dedicatedB = dedicatedbrand.cat('https://www.dedicatedbrand.com', '.mainNavigation-link-subMenu-link.mainNavigation-link-subMenu-link--image', 'dedicated');
+    //const Mud = dedicatedbrand.cat('https://mudjeans.eu/', '.header-nav-link.level-3', 'mud');
+    //const Adresse = dedicatedbrand.cat('https://adresse.paris/630-toute-la-collection?id_category=630&n=109', '.product-name', 'adresse');
   } catch (e) {
     console.error(e);
     process.exit(1);
@@ -18,4 +16,4 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
 
 const [,, eshop] = process.argv;
 
-sandbox(eshop);
+sandbox();
--- server/sources/dedicatedbrand.js
+++ server/sources/dedicatedbrand.js
@@ -6,11 +6,11 @@ const cheerio = require('cheerio');
  * @param  {String} data - html response
  * @return {Array} products
  */
-const parse = data => {
+const parse = (data, brand) => {
   const $ = cheerio.load(data);
-
-  return $('.productList-container .productList')
-    .map((i, element) => {
+  if(brand == "dedicated")
+  {
+    return $('.productList-container .productList').map((i, element) => {
       const name = $(element)
         .find('.productList-title')
         .text()
@@ -25,6 +25,88 @@ const parse = data => {
       return {name, price};
     })
     .get();
+  }
+  if(brand == "mud"){
+    return $('.product-link.product-link__grid').map((i, element) => {
+      const name = $(element)
+        .find('.product-title')
+        .text()
+        .trim()
+        .replace(/\s/g, ' ');
+      const price = 
+        $(element)
+          .find('.product-price')
+          .text()
+
+      return {name, price};
+    })
+    .get();
+  }
+  if(brand == "adresse"){
+    return $('.product-name').map((i, element) => {
+      const name = $(element)
+        .text()
+        .trim()
+        .replace(/\s/g, ' ');
+      const price = 
+        $(element)
+          .find('.product-price')
+          .text()
+
+      return {name, price};
+    })
+    .get();
+  }
+};
+
+const cat = async (link, path, brand) => {
+  const response = await axios(link);
+  const {data, status} = response;
+  const $ = cheerio.load(data);
+  const links = [];
+
+  if(brand == "dedicated")
+  {
+    const category = $(path).map((i, element) => {
+      const l = link + $(element).find('a').attr('href');
+      if(l.includes("/men/") == true)
+      {
+        links.push(l);
+      }
+    });
+    for(const element of links)
+    {
+      const products = await scrape(element, brand);
+      console.log(`🕵️‍♀️  browsing ${element}`);
+      console.log(products);
+      console.log(products.length);
+      console.log('done');
+    } 
+  }
+  if(brand == "mud"){
+    const category = $(path).map((i, element) => {
+      const l = link + $(element).attr('href');
+      if(l.includes("/men-") == true)
+      {
+        links.push(l);
+      }
+    });
+    for(const element of links)
+    {
+      const products = await scrape(element, brand);
+      console.log(`🕵️‍♀️  browsing ${element}`);
+      console.log(products);
+      console.log(products.length);
+      console.log('done');
+    } 
+  }
+  if(brand == "adresse"){
+    const products = await scrape(link, brand);
+    console.log(`🕵️‍♀️  browsing ${link}`);
+    console.log(products);
+    console.log(products.length);
+    console.log('done');
+  }
 };
 
 /**
@@ -32,15 +114,17 @@ const parse = data => {
  * @param  {[type]}  url
  * @return {Array|null}
  */
-module.exports.scrape = async url => {
+const scrape = async (url, brand) => {
   const response = await axios(url);
   const {data, status} = response;
 
   if (status >= 200 && status < 300) {
-    return parse(data);
+    return parse(data, brand);
   }
 
   console.error(status);
 
   return null;
 };
+
+module.exports = { cat, scrape };
